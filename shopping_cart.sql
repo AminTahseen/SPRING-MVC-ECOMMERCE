0e25a5fef587b97c25de18e0d66a04fd69763f79
Amin Tahseen <51921201+AminTahseen@users.noreply.github.com>
2019-07-18 09:27:44 -0700

    Add files via upload

---
+++ shopping_cart.sql
@@ -0,0 +1,92 @@
+/*
+SQLyog Community v11.31 (64 bit)
+MySQL - 5.0.67-community-nt : Database - db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db`;
+
+/*Table structure for table `accounts` */
+
+DROP TABLE IF EXISTS `accounts`;
+
+CREATE TABLE `accounts` (
+  `User_Name` varchar(20) NOT NULL,
+  `Active` bit(1) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `User_Role` varchar(20) NOT NULL,
+  PRIMARY KEY  (`User_Name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `accounts` */
+
+insert  into `accounts`(`User_Name`,`Active`,`Password`,`User_Role`) values ('manager1','','123','MANAGER');
+
+/*Table structure for table `order_details` */
+
+DROP TABLE IF EXISTS `order_details`;
+
+CREATE TABLE `order_details` (
+  `ID` varchar(50) NOT NULL,
+  `Amount` double NOT NULL,
+  `Price` double NOT NULL,
+  `Quanity` int(11) NOT NULL,
+  `ORDER_ID` varchar(50) NOT NULL,
+  `PRODUCT_ID` varchar(20) NOT NULL,
+  PRIMARY KEY  (`ID`),
+  KEY `ORDER_DETAIL_ORD_FK` (`ORDER_ID`),
+  KEY `ORDER_DETAIL_PROD_FK` (`PRODUCT_ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `order_details` */
+
+/*Table structure for table `orders` */
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `ID` varchar(50) NOT NULL,
+  `Amount` double NOT NULL,
+  `Customer_Address` varchar(255) NOT NULL,
+  `Customer_Email` varchar(128) NOT NULL,
+  `Customer_Name` varchar(255) NOT NULL,
+  `Customer_Phone` varchar(128) NOT NULL,
+  `Order_Date` datetime NOT NULL,
+  `Order_Num` int(11) NOT NULL,
+  PRIMARY KEY  (`ID`),
+  UNIQUE KEY `UK_sxhpvsj665kmi4f7jdu9d2791` (`Order_Num`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `orders` */
+
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `Code` varchar(20) NOT NULL,
+  `Create_Date` datetime NOT NULL,
+  `Image` longblob,
+  `Name` varchar(255) NOT NULL,
+  `Price` double NOT NULL,
+  PRIMARY KEY  (`Code`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `products` */
+
+insert  into `products`(`Code`,`Create_Date`,`Image`,`Name`,`Price`) values ('S002','2019-05-09 23:08:36',NULL,'Spring for Beginners',50),('S003','2019-05-09 23:08:36',NULL,'Swift for Beginners',120),('S004','2019-05-09 23:08:36',NULL,'Oracle XML Parser',120),('S005','2019-05-09 23:08:36',NULL,'CSharp Tutorial for Beginers',110);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
